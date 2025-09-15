e2ada60a4d28d5e1899112f630d9f5c410d1b199
Cleverton Neves <clever.nvs@gmail.com>
2025-09-15 17:55:51 -0300

    feat: Tipando as variáveis

--- variables.tf
+++ variables.tf
@@ -1,19 +1,37 @@
+variable "digitalocean_token" {
+  default     = ""
+  type        = string
+  description = "Token da API da Digital Ocean"
+}
+
 variable "droplet_name" {
-  default = "vm-exemplo"
+  default     = "vm-exemplo"
+  type        = string
+  description = "Nome inicial do Droplet"
 }
 
 variable "droplet_region" {
-  default = "nyc2"
+  default     = "nyc2"
+  type        = string
+  description = "Região de criação da infra"
 }
 
 variable "droplet_size" {
-  default = "s-1vcpu-1gb"
+  default     = "s-1vcpu-1gb"
+  type        = string
+  description = "Perfil de máquina dos Droplets"
+
 }
 
 variable "ssh_key" {
-  default = "exemplo-terraform"
+  default     = "exemplo-terraform"
+  type        = string
+  description = "Chave SSH que vai ser utilizada"
 }
 
-variable "digitalocean_token" {
-  default = ""
+variable "vms_count" {
+  default     = 1
+  type        = number
+  description = "Quantidade de máquinas"
 }
+
